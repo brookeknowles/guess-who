a4c7ffd5aa30eb50f796a73f8487228adefafac8
Brooke Knowles <brookeknowles7@gmail.com>
2023-02-22 17:54:41 +1300

    cards are in a grid now

--- src/components/CharacterCard.jsx
+++ src/components/CharacterCard.jsx
@@ -4,7 +4,7 @@ function CharacterCard(props) {
     return (
         <div className="w-1/6 p-2">
         <div className="bg-white rounded-lg shadow-lg overflow-hidden">
-            <img src={props.character.image} alt={props.character.name} className="h-40 w-full object-cover" />
+            <img src={props.character.image} alt={props.character.name} className="h-auto w-auto object-cover" />
         </div>
         </div>
     );
--- src/components/GameBoard.jsx
+++ src/components/GameBoard.jsx
@@ -1,23 +1,21 @@
 import React from "react";
-import CharacterRow from "./CharacterRow";
+import Grid from "@mui/material/Grid";
+import CharacterCard from "./CharacterCard";
 import character_data from "../data/character_data";
 
 function GameBoard() {
-    const characters = character_data
-    const rows = [];
-  
-    for (let i = 0; i < characters.length; i += 6) {
-      const row = characters.slice(i, i + 6);
-      rows.push(row);
-    }
-  
-    return (
-      <div className="container mx-auto">
-        {rows.map((row, index) => (
-          <CharacterRow key={index} characters={row} />
-        ))}
-      </div>
-    );
+  const characters = character_data;
+
+  return (
+    <Grid container spacing={2}>
+      {characters.map((character) => (
+        // <Grid item key={character.id} xs={6} sm={4} md={3}>
+        <Grid item xs={3} sx={{ width: "100%", height: "100%" }}>
+          <CharacterCard character={character} />
+        </Grid>
+      ))}
+    </Grid>
+  );
 }
 
-export default GameBoard;  
+export default GameBoard;
