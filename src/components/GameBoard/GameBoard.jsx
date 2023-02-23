abfd601f9976365e78e5d5ea9f915e0cc04740c2
Brooke Knowles <brookeknowles7@gmail.com>
2023-02-23 20:02:43 +1300

    make the cards closer together

--- src/components/GameBoard/GameBoard.jsx
+++ src/components/GameBoard/GameBoard.jsx
@@ -1,16 +1,16 @@
 import React from "react";
 import Grid from "@mui/material/Grid";
-import "./GameBoard.css";
 import CharacterCard from "../CharacterCard/CharacterCard";
 import character_data from "../../data/character_data";
+import "./GameBoard.css";
 
 function GameBoard() {
   const characters = character_data;
 
   return (
-    <Grid container spacing={1} className="game-board">
+    <Grid container spacing={4} className="game-board">
       {characters.map((character) => (
-        <Grid item xs={12} sm={6} md={4} lg={3} key={character.id}>
+        <Grid item key={character.id} className="game-board__card">
           <CharacterCard character={character} />
         </Grid>
       ))}
