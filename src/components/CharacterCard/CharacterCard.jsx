324ad66ea56cdcb0721009cbca72e7cbfb801355
Brooke Knowles <brookeknowles7@gmail.com>
2023-06-03 13:04:00 +1200

    flipping the cards

--- src/components/CharacterCard/CharacterCard.jsx
+++ src/components/CharacterCard/CharacterCard.jsx
@@ -1,18 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
 import "./CharacterCard.css";
 
 function CharacterCard(props) {
-  return (
-    <div className="character-card">
-      <div>
-        <img
-          src={props.character.image}
-          alt={props.character.name}
-          style={{ maxHeight: "8rem" }}
-        />
-      </div>
-    </div>
-  );
+    const [isFlipped, setIsFlipped] = useState(false);
+
+    // Flip over the card when user clicks on it
+    const handleClick = () => {
+        setIsFlipped(!isFlipped);
+    };
+
+    return (
+        <div className="character-card" onClick={handleClick}>
+            <div>
+                <img
+                    src={isFlipped ? "/blank_cards/red_blank.png" : props.character.image}
+                    alt={props.character.name}
+                    style={{ maxHeight: "8rem" }}
+                />
+            </div>
+        </div>
+    );
 }
 
 export default CharacterCard;
