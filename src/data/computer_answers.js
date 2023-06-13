396e7b55247bfb53ebc32d7da4f1dc7407f50ae6
Brooke Knowles <brookeknowles7@gmail.com>
2023-06-13 22:00:58 +1200

    more computer answers

--- src/data/computer_answers.js
+++ src/data/computer_answers.js
@@ -13,7 +13,25 @@ const computerAnswers = {
         no: ["Bad Bunny", "Drake", "Ed Sheeran", "Justin Bieber", "Shawn Mendes", "The Weeknd", "Zayn", "Adele",
             "Dua Lipa", "Rihanna"]
     },
-    // Add more questions and character lists here
+    "Is the character married?": {
+        yes: ["Joe Jonas", "Justin Timberlake", "Nick Jonas", "Ariana Grande", "Beyonce", "Ed Sheeran", "Justin Bieber",
+            "Britney Spears", ],
+        no: ["Bruno Mars", "Demi Lovato", "Doja Cat", "Machine Gun Kelly", "Bad Bunny", "Drake", "Shawn Mendes",
+            "The Weeknd", "Zayn", "Adele", "Dua Lipa", "Rihanna", "Katy Perry", "Lady Gaga", "Miley Cyrus",
+            "Taylor Swift"]
+    },
+    "Does the character have children?": {
+        yes: ["Joe Jonas", "Justin Timberlake", "Machine Gun Kelly", "Nick Jonas",
+             "Beyonce", "Britney Spears", "Katy Perry", "Drake", "Ed Sheeran", "Zayn", "Adele", "Rihanna"],
+        no: ["Ariana Grande", "Lady Gaga", "Miley Cyrus", "Taylor Swift", "Bruno Mars", "Demi Lovato", "Doja Cat",
+            "Bad Bunny", "Justin Bieber", "Shawn Mendes", "The Weeknd", "Dua Lipa"]
+    },
+    "Has the character ever been part of a band/group?": {
+        yes: ["Joe Jonas", "Justin Timberlake", "Nick Jonas", "Beyonce", "Zayn"],
+        no: ["Machine Gun Kelly", "Britney Spears", "Katy Perry", "Drake", "Ed Sheeran", "Adele", "Rihanna",
+            "Ariana Grande", "Lady Gaga", "Miley Cyrus", "Taylor Swift", "Bruno Mars", "Demi Lovato", "Doja Cat",
+            "Bad Bunny", "Justin Bieber", "Shawn Mendes", "The Weeknd", "Dua Lipa"]
+    }
 };
 
 export default computerAnswers;
