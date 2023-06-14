b8c7951c1be854409d78f86ddb16896a91a4bb00
Brooke Knowles <brookeknowles7@gmail.com>
2023-06-14 20:21:04 +1200

    add more questions

--- src/data/computer_answers.js
+++ src/data/computer_answers.js
@@ -6,6 +6,13 @@ const computerAnswers = {
         no: ["Adele", "Ariana Grande", "Beyonce", "Britney Spears", "Demi Lovato",
             "Doja Cat", "Dua Lipa", "Katy Perry", "Lady Gaga", "Miley Cyrus", "Rihanna", "Taylor Swift"]
     },
+    "Is it a female character?": {
+        yes: ["Adele", "Ariana Grande", "Beyonce", "Britney Spears", "Demi Lovato",
+            "Doja Cat", "Dua Lipa", "Katy Perry", "Lady Gaga", "Miley Cyrus", "Rihanna", "Taylor Swift"],
+        no: ["Bad Bunny", "Bruno Mars", "Drake", "Ed Sheeran", "Joe Jonas",
+            "Justin Bieber", "Justin Timberlake", "Machine Gun Kelly", "Nick Jonas",
+            "Shawn Mendes", "The Weeknd", "Zayn"]
+    },
     "Is the character American?": {
         yes: ["Bruno Mars", "Joe Jonas", "Justin Timberlake", "Machine Gun Kelly", "Nick Jonas",
             "Ariana Grande", "Beyonce", "Britney Spears", "Demi Lovato", "Doja Cat","Katy Perry",
@@ -15,7 +22,7 @@ const computerAnswers = {
     },
     "Is the character married?": {
         yes: ["Joe Jonas", "Justin Timberlake", "Nick Jonas", "Ariana Grande", "Beyonce", "Ed Sheeran", "Justin Bieber",
-            "Britney Spears", ],
+            "Britney Spears"],
         no: ["Bruno Mars", "Demi Lovato", "Doja Cat", "Machine Gun Kelly", "Bad Bunny", "Drake", "Shawn Mendes",
             "The Weeknd", "Zayn", "Adele", "Dua Lipa", "Rihanna", "Katy Perry", "Lady Gaga", "Miley Cyrus",
             "Taylor Swift"]
@@ -31,7 +38,13 @@ const computerAnswers = {
         no: ["Machine Gun Kelly", "Britney Spears", "Katy Perry", "Drake", "Ed Sheeran", "Adele", "Rihanna",
             "Ariana Grande", "Lady Gaga", "Miley Cyrus", "Taylor Swift", "Bruno Mars", "Demi Lovato", "Doja Cat",
             "Bad Bunny", "Justin Bieber", "Shawn Mendes", "The Weeknd", "Dua Lipa"]
-    }
+    },
+    "Were they ever a Disney star?": {
+        yes: ["Justin Timberlake", "Britney Spears", "Joe Jonas", "Nick Jonas", "Miley Cyrus", "Demi Lovato"],
+        no: ["Beyonce", "Zayn", "Machine Gun Kelly", "Katy Perry", "Drake", "Ed Sheeran", "Adele", "Rihanna",
+            "Ariana Grande", "Lady Gaga", "Taylor Swift", "Bruno Mars", "Doja Cat", "Bad Bunny", "Justin Bieber",
+            "Shawn Mendes", "The Weeknd", "Dua Lipa"]
+    },
 };
 
 export default computerAnswers;
