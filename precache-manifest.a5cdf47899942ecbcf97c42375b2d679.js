9bb5273106bd0a916440b8691a588185599630ab
Brooke Knowles <brookeknowles7@gmail.com>
2023-09-22 21:21:29 +1200

    Updates

---
+++ precache-manifest.a5cdf47899942ecbcf97c42375b2d679.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "68d3add25f4079bb63e2",
+    "url": "/guess-who/static/css/main.49129aa4.chunk.css"
+  },
+  {
+    "revision": "68d3add25f4079bb63e2",
+    "url": "/guess-who/static/js/main.68d3add2.chunk.js"
+  },
+  {
+    "revision": "9a53afc1bdedb8c2f4c8",
+    "url": "/guess-who/static/js/1.9a53afc1.chunk.js"
+  },
+  {
+    "revision": "54d5c85bc075d61d74ef",
+    "url": "/guess-who/static/js/runtime~main.54d5c85b.js"
+  },
+  {
+    "revision": "02a6b67ce38fc19a62904eca73eced66",
+    "url": "/guess-who/index.html"
+  }
+];
