185cc66fc027d02fbaf800059f0d94d22a686784
Brooke Knowles <brookeknowles7@gmail.com>
2023-09-22 20:23:25 +1200

    formatting

--- src/components/CharacterCard/CharacterCard.jsx
+++ src/components/CharacterCard/CharacterCard.jsx
@@ -1,27 +1,3 @@
-// import React, { useState } from "react";
-// import "./CharacterCard.css";
-
-// function CharacterCard(props) {
-//     const [isFlipped, setIsFlipped] = useState(false);
-
-//     const handleClick = () => {
-//         setIsFlipped(!isFlipped);
-//     };
-
-//     return (
-//         <div className="character-card" onClick={handleClick}>
-//             <div>
-//                 <img
-//                     src={isFlipped ? "/blank_cards/red_blank.png" : props.character.image}
-//                     alt={props.character.name}
-//                     style={{ maxHeight: "8rem" }}
-//                 />
-//             </div>
-//         </div>
-//     );
-// }
-
-// export default CharacterCard;
 import React, { useState, useEffect } from "react";
 import "./CharacterCard.css";
 
